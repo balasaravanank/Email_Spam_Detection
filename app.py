61cbe49b81fdee9e915743295037fbc945e2a5b9
Bala Saravanan K <130923584+balasaravanank@users.noreply.github.com>
2024-12-30 12:47:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -151,4 +151,4 @@ with st.container():
     st.markdown('</div>', unsafe_allow_html=True)
 
 # Footer
-st.markdown(f'<div class="footer"><p>Developed by <b>CHANDRU,GOAT FOR A REASON> | © 2024 Gmail Spam Detection System</p></div>', unsafe_allow_html=True)
+st.markdown(f'<div class="footer"><p>Developed by <b>Bala Saravanan K> | © 2024 Gmail Spam Detection System</p></div>', unsafe_allow_html=True)
